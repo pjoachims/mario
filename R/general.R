195b5be8b81fc9b9dcadda2736c06ac209874b2a
Per Joachims <p.joachims@gmail.com>
2020-06-03 09:27:55 +0200

    test exportability

--- R/general.R
+++ R/general.R
@@ -1,8 +1,27 @@
+#' Plot sales of Mario games.
+#'
+#' @param type Type of plot ("bar" or "pie").
+#' @return ggplot of sales
+#'
+#' @export
+#'
+#' @examples
+#' mario_plot_sales("bar")
+#' mario_plot_sales("pie")
 mario <- function() {
   print("It's-a Me, Mario!")
 }
 
-
+#' Plot sales of Mario games. WABBALUBBADUBDUB
+#'
+#' @param type Type of plot ("bar" or "pie").
+#' @return ggplot of sales
+#'
+#' @export
+#'
+#' @examples
+#' mario_plot_sales("bar")
+#' mario_plot_sales("pie")
 best_friend <- function(version) {
   bf_data <- bestfriend
   name <- bf_data[bf_data$Version == version, "Name"]
